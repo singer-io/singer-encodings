189233078ddbaa378394e7d9e480bd1e15173878
Kyle Allan <kyle@stitchdata.com>
2018-07-26 16:02:00 -0400

    add csv encoding

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import subprocess
+
+setup(name="singer-encodings",
+      version='0.0.1',
+      description="Singer.io encodings library",
+      author="Stitch",
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      url="http://singer.io",
+      install_requires=[
+      ],
+      packages=find_packages(),
+)
---
+++ singer_encodings/csv.py
@@ -0,0 +1,43 @@
+import codecs
+import csv
+
+
+def generator_wrapper(reader):
+    for row in reader:
+        to_return = {}
+        for key, value in row.items():
+            if key is None:
+                key = '_s3_extra'
+
+            formatted_key = key
+
+            # remove non-word, non-whitespace characters
+            #formatted_key = re.sub(r"[^\w\s]", '', formatted_key)
+
+            to_return[formatted_key] = value
+
+        yield to_return
+
+
+def get_row_iterator(iterable, options=None):
+    file_stream = codecs.iterdecode(iterable, encoding='utf-8')
+
+    field_names = None
+
+    # Replace any NULL bytes in the line given to the DictReader
+    reader = csv.DictReader((line.replace('\0', '') for line in file_stream), fieldnames=field_names)
+
+    headers = set(reader.fieldnames)
+    if options['key_properties']:
+        key_properties = set(options['key_properties'])
+        if not key_properties.issubset(headers):
+            raise Exception('CSV file missing required headers: {}, file only contains headers for fields: {}'
+                            .format(key_properties - headers, headers))
+
+    if options['date_overrides']:
+        date_overrides = set(options['date_overrides'])
+        if not date_overrides.issubset(headers):
+            raise Exception('CSV file missing date_overrides headers: {}, file only contains headers for fields: {}'
+                            .format(date_overrides - headers, headers))
+
+    return generator_wrapper(reader)
