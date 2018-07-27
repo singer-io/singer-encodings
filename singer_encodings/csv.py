0c9e9aba6ac4fe9ee7effdfeea8e1c4eaba8c59b
Kyle Allan <kyle@stitchdata.com>
2018-07-27 15:21:25 -0400

    fix options

--- singer_encodings/csv.py
+++ singer_encodings/csv.py
@@ -20,6 +20,7 @@ def generator_wrapper(reader):
 
 
 def get_row_iterator(iterable, options=None):
+    options = options or {}
     file_stream = codecs.iterdecode(iterable, encoding='utf-8')
 
     field_names = None
@@ -28,13 +29,13 @@ def get_row_iterator(iterable, options=None):
     reader = csv.DictReader((line.replace('\0', '') for line in file_stream), fieldnames=field_names)
 
     headers = set(reader.fieldnames)
-    if options['key_properties']:
+    if options.get('key_properties'):
         key_properties = set(options['key_properties'])
         if not key_properties.issubset(headers):
             raise Exception('CSV file missing required headers: {}, file only contains headers for fields: {}'
                             .format(key_properties - headers, headers))
 
-    if options['date_overrides']:
+    if options.get('date_overrides'):
         date_overrides = set(options['date_overrides'])
         if not date_overrides.issubset(headers):
             raise Exception('CSV file missing date_overrides headers: {}, file only contains headers for fields: {}'
