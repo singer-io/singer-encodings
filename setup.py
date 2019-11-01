838f02379ca23ec244cc938f511f6d38e1495825
cosimon <cosimon@users.noreply.github.com>
2019-11-01 15:34:58 -0400

    Bump to v0.0.5 (#5)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import subprocess
 
 setup(name="singer-encodings",
-      version='0.0.4',
+      version='0.0.5',
       description="Singer.io encodings library",
       author="Stitch",
       classifiers=['Programming Language :: Python :: 3 :: Only'],
