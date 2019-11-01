6229ab1961f30e3686fd7d4d3091c500140a741d
Collin Simon <csimon@talend.com>
2019-11-01 14:58:42 +0000

    Add support for zip files

--- singer_encodings/compression.py
+++ singer_encodings/compression.py
@@ -1,4 +1,5 @@
 import gzip
+import zipfile
 
 def infer(iterable, file_name):
     """Uses the incoming file_name and checks the end of the string
@@ -8,7 +9,11 @@ def infer(iterable, file_name):
 
     if file_name.endswith('.tar.gz'):
         raise NotImplementedError("tar.gz not supported")
-    elif file_name.endswith(".gz"):
+    elif file_name.endswith('.gz'):
         yield gzip.GzipFile(fileobj=iterable)
+    elif file_name.endswith('.zip'):
+        with zipfile.ZipFile(iterable) as zip:
+            for name in zip.namelist():
+                yield zip.open(name)
     else:
         yield iterable
