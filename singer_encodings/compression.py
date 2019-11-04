a46c9f4977678da04b4897632b26cd4927434820
Collin Simon <csimon@talend.com>
2019-11-04 15:17:40 +0000

    WIP: Add tar support

--- singer_encodings/compression.py
+++ singer_encodings/compression.py
@@ -1,5 +1,6 @@
 import gzip
 import zipfile
+import tarfile
 
 def infer(iterable, file_name):
     """Uses the incoming file_name and checks the end of the string
@@ -7,8 +8,10 @@ def infer(iterable, file_name):
     if not file_name:
         raise Exception("Need file name")
 
-    if file_name.endswith('.tar.gz'):
-        raise NotImplementedError("tar.gz not supported")
+    if file_name.endswith('.tar.gz') or file_name.endswith('.tar.bz2'):
+        with tarfile.open(fileobj=iterable, mode='r') as tar:
+            for tarmember in tar.getmembers():
+                yield tar.extractfile(tarmember)
     elif file_name.endswith('.gz'):
         yield gzip.GzipFile(fileobj=iterable)
     elif file_name.endswith('.zip'):
