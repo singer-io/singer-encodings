306946f17228adad2022ae93fe2aef4045ff6f88
Collin Simon <csimon@talend.com>
2019-11-05 19:24:53 +0000

    json_schema needs to account for delimiter

--- singer_encodings/json_schema.py
+++ singer_encodings/json_schema.py
@@ -37,6 +37,7 @@ def sample_file(conn, table_spec, f, sample_rate, max_records):
 
     # Add file_name to opts and flag infer_compression to support gzipped files
     opts = {'key_properties': table_spec['key_properties'],
+            'delimiter': table_spec['delimiter'],
             'file_name': f['filepath']}
 
     readers = csv.get_row_iterators(file_handle, options=opts, infer_compression=True)
