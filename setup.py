0f9dbd3fea7157337457b216d6ba1c87f83e062c
Karan Panchal (C) <karan.panchal@CDSYS.LOCAL>
2021-05-18 21:07:52 +0530

    TDL-12464: Added test cases to validate the support of duplicate headers added in singer-encodings library

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import subprocess
 
 setup(name="singer-encodings",
-      version='0.0.8',
+      version='0.0.9',
       description="Singer.io encodings library",
       author="Stitch",
       classifiers=['Programming Language :: Python :: 3 :: Only'],
--- tests/test_csv.py
+++ tests/test_csv.py
@@ -1,5 +1,7 @@
 import unittest
+from unittest import mock
 from singer_encodings import csv
+from singer_encodings.csv_helper import CSVHelper
 
 class TestRestKey(unittest.TestCase):
 
@@ -17,7 +19,7 @@ class TestNullBytes(unittest.TestCase):
     def test(self):
         row_iterator = csv.get_row_iterator(self.csv_data)
         rows = [r for r in row_iterator]
-        self.assertEqual(rows[0]['columnA'], '1')
+        self.assertEqual(rows[0]['columnB'], '2')
 
 class TestOptions(unittest.TestCase):
 
@@ -37,3 +39,83 @@ class TestOptions(unittest.TestCase):
 
         with self.assertRaises(Exception):
             row_iterator = csv.get_row_iterator(self.csv_data, options={'date_overrides': ['fizz']})
+
+class TestRestKeyWithDuplicateHeader(unittest.TestCase):
+
+    csv_data = [b"columnA,columnB,columnC", b"1,2,3,4"]
+
+    def test(self):
+        row_iterator = csv.get_row_iterator(self.csv_data, None, None, True)
+        rows = [r for r in row_iterator]
+        self.assertEqual(rows[0]['_sdc_extra'], ["{\"no_headers\": [\"4\"]}"])
+
+class TestRestValue(unittest.TestCase):
+
+    csv_data = [b"columnA,columnB,columnC", b"1,2"]
+
+    def test(self):
+        row_iterator = csv.get_row_iterator(self.csv_data, None, None, True)
+        rows = [r for r in row_iterator]
+        self.assertEqual(list(rows[0].keys()), ["columnA","columnB"])
+
+class TestDuplicateHeaders(unittest.TestCase):
+
+    csv_data = [b"columnA,columnB,columnC,columnB,columnC,columnC", b"1,2,3,4,5,6"]
+
+    def test(self):
+        row_iterator = csv.get_row_iterator(self.csv_data, None, None, True)
+        rows = [r for r in row_iterator]
+        self.assertEqual(rows[0]['_sdc_extra'], ["{\"columnB\": \"4\"}","{\"columnC\": [\"5\", \"6\"]}"])
+        self.assertEqual(list(rows[0].keys()), ["columnA","columnB","columnC","_sdc_extra"])
+
+class TestDuplicateHeadersRestKey(unittest.TestCase):
+
+    csv_data = [b"columnA,columnB,columnC,columnB,columnC,columnC", b"1,2,3,4,5,6,7,8,9"]
+
+    def test(self):
+        row_iterator = csv.get_row_iterator(self.csv_data, None, None, True)
+        rows = [r for r in row_iterator]
+        self.assertEqual(rows[0]['_sdc_extra'],  ["{\"no_headers\": [\"7\", \"8\", \"9\"]}","{\"columnB\": \"4\"}","{\"columnC\": [\"5\", \"6\"]}"])
+        self.assertEqual(list(rows[0].keys()), ["columnA","columnB","columnC","_sdc_extra"])
+
+class TestDuplicateHeadersRestValue(unittest.TestCase):
+
+    csv_data = [b"columnA,columnB,columnC,columnB,columnC,columnC", b"1,2,3,4,5"]
+
+    def test(self):
+        row_iterator = csv.get_row_iterator(self.csv_data, None, None, True)
+        rows = [r for r in row_iterator]
+        self.assertEqual(rows[0]['_sdc_extra'], ["{\"columnB\": \"4\"}","{\"columnC\": \"5\"}"])
+        self.assertEqual(list(rows[0].keys()), ["columnA","columnB","columnC","_sdc_extra"])
+
+class TestDuplicateHeadersRestValueNoSDCExtra(unittest.TestCase):
+
+    csv_data = [b"columnA,columnB,columnC,columnB,columnC,columnC", b"1,2,3"]
+
+    def test(self):
+        row_iterator = csv.get_row_iterator(self.csv_data, None, None, True)
+        rows = [r for r in row_iterator]
+        self.assertEqual(list(rows[0].keys()), ["columnA","columnB","columnC"])
+
+class TestMissingFiedInCatalog(unittest.TestCase):
+
+    csv_data = [b"columnA,columnB,columnC,columnB,columnC,columnC", b"1,2,3,4,5,6"]
+
+    def test(self):
+        row_iterator = csv.get_row_iterator(self.csv_data, None, ["columnA","columnB"], True)
+        rows = [r for r in row_iterator]
+        self.assertListEqual(rows[0]['_sdc_extra'], ["{\"columnC\": [\"3\", \"5\", \"6\"]}","{\"columnB\": \"4\"}",])
+        self.assertEqual(list(rows[0].keys()), ["columnA","columnB","_sdc_extra"])
+
+
+class TestWarningForDupHeaders(unittest.TestCase):
+    
+    csv_data = [b"columnA,columnB,columnC,columnC", b"1,2,3"]
+
+    @mock.patch("singer_encodings.csv_helper.LOGGER.warn")
+    def test(self, mocked_logger_warn):
+        row_iterator = csv.get_row_iterator(self.csv_data, None, None, True)
+        rows = [r for r in row_iterator]
+        self.assertEqual(list(rows[0].keys()), ["columnA","columnB","columnC"])
+
+        mocked_logger_warn.assert_called_with('Duplicate Header(s) %s found in the csv and its value will be stored in the \"_sdc_extra\" field.', {'columnC'})
