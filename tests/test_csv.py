e560878ed25d787564c7f322feb5409872c8c4f6
dbshah1212 <root@ubuntu16.cdsys.local>
2021-05-21 15:31:04 +0530

    debug

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -56,9 +56,6 @@ class TestRestValue(unittest.TestCase):
     def test(self):
         row_iterator = csv.get_row_iterator(self.csv_data, None, None, True)
         rows = [r for r in row_iterator]
-        print(">>>>>>>>>>>>")
-        print(rows)
-        print(rows[0].keys())
         self.assertEqual(list(rows[0].keys()), ["columnA","columnB"])
 
 class TestDuplicateHeaders(unittest.TestCase):
@@ -119,6 +116,9 @@ class TestWarningForDupHeaders(unittest.TestCase):
     def test(self, mocked_logger_warn):
         row_iterator = csv.get_row_iterator(self.csv_data, None, None, True)
         rows = [r for r in row_iterator]
+        print(">>>>>>>>>>>>")
+        print(rows)
+        print(rows[0].keys())
         self.assertEqual(list(rows[0].keys()), ["columnA","columnB","columnC"])
 
         mocked_logger_warn.assert_called_with('Duplicate Header(s) %s found in the csv and its value will be stored in the \"_sdc_extra\" field.', {'columnC'})
