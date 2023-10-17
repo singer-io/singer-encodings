6c7ebe29d60bcbd522c3974eede7b88e72f4388b
Sourabh Gandhi <sgandhi@talend.com>
2023-10-17 12:24:35 +0000

    remove the spaces in between doc string lines

--- singer_encodings/utils.py
+++ singer_encodings/utils.py
@@ -6,17 +6,13 @@ LOGGER = singer.get_logger()
 def is_valid_encoding(encoding_format):
     """
     Check if the provided encoding format is valid.
-
     Args:
         encoding_format (str): The encoding format to be validated.
-
     Returns:
         bool: True if the encoding format is valid, False otherwise.
-
     This function attempts to look up the specified encoding format using Python's `codecs.lookup()`
     method. If a LookupError occurs, indicating an invalid encoding format, a warning is logged, and
     False is returned. Otherwise, True is returned, indicating a valid encoding format.
-
     """
     try:
         # Attempt to look up the codecs for the specified encoding format
