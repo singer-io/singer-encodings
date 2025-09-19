83e45b656c9ce54aa20888c87c013dcf59875767
Dylan Sprayberry <dylan.sprayberry@qlik.com>
2025-09-19 17:59:11 +0000

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import subprocess
 
 setup(name="singer-encodings",
-      version='0.1.4',
+      version='0.1.5',
       description="Singer.io encodings library",
       author="Stitch",
       classifiers=['Programming Language :: Python :: 3 :: Only'],
