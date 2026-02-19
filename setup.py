424536020543d8930c87b21a8f1f2597a7c5cefd
Mukesh Bhatt <mukesh.bhatt@qlik.com>
2026-02-19 15:14:42 +0530

    Update setup.py
    Co-authored-by: Rushikesh Todkar <98420315+RushiT0122@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import subprocess
 
 setup(name="singer-encodings",
-      version='0.3.1',
+      version='0.4.0',
       description="Singer.io encodings library",
       author="Stitch",
       classifiers=['Programming Language :: Python :: 3 :: Only'],
